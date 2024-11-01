59aba190aedfeb3a6587456a81bb46f3dba378c8
Luiz Benevenuto <luiz.benevenuto@gmail.com>
2024-11-01 10:57:39 +0100

    Initial commit

---
+++ src/lib/server/db/schema.ts
@@ -0,0 +1,9 @@
+import { sql } from 'drizzle-orm';
+import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
+
+export const users = sqliteTable('users', {
+	id: text('id').primaryKey(),
+	createdAt: integer('created_at', { mode: 'timestamp' }).default(sql`(unixepoch())`),
+	updatedAt: integer('updated_at', { mode: 'timestamp' }).default(sql`(unixepoch())`),
+	deletedAt: integer('deleted_at', { mode: 'timestamp' })
+});
